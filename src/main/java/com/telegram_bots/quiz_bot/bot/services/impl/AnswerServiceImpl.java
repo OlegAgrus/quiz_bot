5ae1e4ad68cb0280f54e9fdce2cc16dfdb815245
YmiR1710 <voliansyi@ucu.edu.ua>
2020-07-29 17:31:27 +0300

    minor change

--- src/main/java/com/telegram_bots/quiz_bot/bot/services/impl/AnswerServiceImpl.java
+++ src/main/java/com/telegram_bots/quiz_bot/bot/services/impl/AnswerServiceImpl.java
@@ -1,6 +1,8 @@
 package com.telegram_bots.quiz_bot.bot.services.impl;
 
 import com.telegram_bots.quiz_bot.bot.services.AnswerService;
+import org.jvnet.hk2.annotations.Service;
 
+@Service
 public class AnswerServiceImpl implements AnswerService {
 }
