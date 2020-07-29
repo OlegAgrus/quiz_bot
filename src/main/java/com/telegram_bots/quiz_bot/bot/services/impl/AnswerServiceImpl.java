c7b83642382cf411c15b9ca5f936148240860432
YmiR1710 <voliansyi@ucu.edu.ua>
2020-07-29 17:26:32 +0300

    templates

---
+++ src/main/java/com/telegram_bots/quiz_bot/bot/services/impl/AnswerServiceImpl.java
@@ -0,0 +1,6 @@
+package com.telegram_bots.quiz_bot.bot.services.impl;
+
+import com.telegram_bots.quiz_bot.bot.services.AnswerService;
+
+public class AnswerServiceImpl implements AnswerService {
+}
---
+++ src/main/java/com/telegram_bots/quiz_bot/bot/services/impl/QuestionServiceImpl.java
@@ -0,0 +1,6 @@
+package com.telegram_bots.quiz_bot.bot.services.impl;
+
+import com.telegram_bots.quiz_bot.bot.services.QuestionService;
+
+public class QuestionServiceImpl implements QuestionService {
+}
